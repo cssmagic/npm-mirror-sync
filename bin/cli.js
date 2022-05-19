7dfef0a15642678003af7542447ae454bcdbd074
cssmagic <cssmagic.cn@gmail.com>
2022-05-19 16:23:12 +0800

    feat: implement basic function

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 'use strict'
 
-console.log('OK')
+const urlPrefix = 'https://npmmirror.com/sync/'
+const pkgName = process.env.npm_package_name
+
+if (!pkgName) {
+	const urlReadme = 'https://github.com/cssmagic/npm-mirror-sync#readme'
+	console.error('[npm-mirror-sync] Error 404: Package name is missing!')
+	console.info('[npm-mirror-sync] See README for more information: ' + urlReadme)
+	process.exit(1)
+}
+
+const url = urlPrefix + pkgName
+const open = require('open')
+console.log('[npm-mirror-sync] Trying to open a web page to trigger syncing...')
+open(url)
+	.then(() => {
+		console.log('[npm-mirror-sync] Done. Please check out the web page for results.')
+	})
+	.catch((err) => {
+		console.error('[npm-mirror-sync] Error: ' + err.message || '(unknown error)')
+	})
